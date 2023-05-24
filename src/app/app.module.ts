d0dee2ef335dbaaaf689d42cb69918ee6fcf3fdb
Jampool G <jampoolgarcia@gmail.com>
2023-05-23 22:46:47 -0400

    created structure project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { QuestionComponent } from './components/question/question.component';
+import { AnswersComponent } from './components/answers/answers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    NavbarComponent,
+    QuestionComponent,
+    AnswersComponent
   ],
   imports: [
     BrowserModule,
