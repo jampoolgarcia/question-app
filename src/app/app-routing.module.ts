073a1da73af1201f878e973353dd816cc3b34b87
Jampool G <jampoolgarcia@gmail.com>
2023-05-23 22:59:55 -0400

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { QuestionComponent } from './components/question/question.component';
+import { AnswersComponent } from './components/answers/answers.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'question', component: QuestionComponent },
+  { path: 'answers', component:  AnswersComponent },
+  { path: '', redirectTo: '/dashboard',  pathMatch: 'full'},
+  { path: '**', redirectTo: '/dashboard' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
